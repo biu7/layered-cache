59ad6c9dfb60c66516c7f9d57defed3de5035a38
潘佳佳 <panjiajia@qimao.com>
2025-07-14 14:17:05 +0800

    update cache impl

--- cache_typed.go
+++ cache_typed.go
@@ -19,8 +19,24 @@ func (c *TypedCache[T]) Get(ctx context.Context, key string, opts ...GetOption) 
 	return &result, nil
 }
 
-func MGet[T any](cache Cache, ctx context.Context, keys []string, opts ...GetOption) (map[string]T, error) {
+func (c *TypedCache[T]) MGet(ctx context.Context, keys []string, opts ...GetOption) (map[string]T, error) {
 	var result map[string]T
-	err := cache.MGet(ctx, keys, &result, opts...)
+	err := c.cache.MGet(ctx, keys, &result, opts...)
 	return result, err
+}
+
+func (c *TypedCache[T]) Set(ctx context.Context, key string, value T, opts ...SetOption) error {
+	return c.cache.Set(ctx, key, value, opts...)
+}
+
+func (c *TypedCache[T]) MSet(ctx context.Context, keyValues map[string]T, opts ...SetOption) error {
+	convertedValues := make(map[string]any, len(keyValues))
+	for key, value := range keyValues {
+		convertedValues[key] = value
+	}
+	return c.cache.MSet(ctx, convertedValues, opts...)
+}
+
+func (c *TypedCache[T]) Delete(ctx context.Context, key string) error {
+	return c.cache.Delete(ctx, key)
 }
